1359998163cacb851545fd64dfe78d58e64c4fa5
WiliTheSparrow <w.julia91@gmail.com>
2019-06-26 13:58:34 +0200

    started SPA

---
+++ script.js
@@ -0,0 +1,15 @@
+var app = new Vue ({
+    el: '#app',
+    
+    data: {
+        path: 1,
+    },
+    
+    methods: {
+        
+        pathChange: function(pathNoChange){
+            this.path = pathNoVhange;
+        }
+    }
+    
+})
