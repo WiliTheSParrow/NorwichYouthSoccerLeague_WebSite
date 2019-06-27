e389935f656e2f5a519069753b95874f7266144a
WiliTheSparrow <w.julia91@gmail.com>
2019-06-27 17:08:03 +0200

    working collapse menu 2

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ var app = new Vue ({
     el: '#app',
     
     data: {
-        path: 6,
+        path: 1,
+        collapseMenuPath: 0,
     },
     
     methods: {
+        
         pathChanger: function(newPath){
-            this.path = newPath;
+            this.path = newPath;  
+        },
+        
+        collapseMenuCahnger: function(newMenu){
+            this.collapseMenuPath = newMenu;
         }
     }
     
