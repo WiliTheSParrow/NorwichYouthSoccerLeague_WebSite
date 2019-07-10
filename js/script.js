9d3f8ccc4eaa0fba4e537f89775c6bddc0057f45
WiliTheSparrow <w.julia91@gmail.com>
2019-07-10 10:39:32 +0200

    finished

--- js/script.js
+++ js/script.js
@@ -1,34 +1,25 @@
+//Initializing Vue object:
 var app = new Vue({
     el: '#app',
-
-
     data: {
         path: 0,
         pathTitle: ["Welcome to Norwich Youth Soccer League! ⚽", "About Norwich Youth Soccer League", "Norwich Youth Soccer League Contact Information", "Norwich Youth Soccer League Game Information", "Registration Form", "Rules of Play & Policies"],
         collapseMenuPath: 0,
         show: true,
     },
-
-
     methods: {
-
         pathChanger: function (newPath) {
             this.path = newPath;
             this.show = false;
-
         },
-
         collapseMenuChanger: function (newMenu) {
             this.collapseMenuPath = newMenu;
         },
     }
 })
-
-
 //Scroll To Top Button:
 if (location.pathname != "show_data.html") {
     $(document).ready(function () {
-
         $(window).scroll(function () {
             if ($(this).scrollTop() > 40) {
                 $('#topBtn').fadeIn();
@@ -36,7 +27,6 @@ if (location.pathname != "show_data.html") {
                 $('#topBtn').fadeOut();
             }
         });
-
         $("#topBtn").click(function () {
             $('html , body').animate({
                 scrollTop: 0
